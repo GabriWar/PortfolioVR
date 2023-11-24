514d8daa7be08a39667fcbe6cb015527f24dbc8f
Gabriel Duarte Guerra <gabrielduarteg02@gmail.com>
2023-11-24 18:32:18 -0300

    fixed torch effect and mobile sizing and visibility

--- reactsndbx/src/portifolio.jsx
+++ reactsndbx/src/portifolio.jsx
@@ -4,31 +4,35 @@ import { useState } from "react";
 import animateonmouseover from "./AnimaTxt";
 import "./hoveranim.css";
 import generateRandomString from "./randomstring";
+
 function Portifolio() {
-
-
 	const [bg, setBg] = useState("GabriWar".repeat(1000));
 
-	function handleMouseMove() {
+	function handleMouseMove(event) {
+		const mouseX = event.clientX;
+		const mouseY = event.clientY;
+		// Use mouseX and mouseY as needed
 		setBg(generateRandomString(3000));
+		document.documentElement.style.setProperty("--x", `${mouseX}px`);
+		document.documentElement.style.setProperty("--y", `${mouseY}px`);
 	}
 
 	return (
-
 		<div className="bg" onMouseMove={handleMouseMove}>
-				<div
-					id="animate"
-					className="name" href="https://github.com/GabriWar"
-					onMouseOver={() => {
-						animateonmouseover("animate", "Gabriel", "Guerra");
-					}}
-				>
-					GABRIWAR
-				</div>
-				{bg}
-			</div>
+						<div className="hover"></div>
 
-		
+			<div
+				id="animate"
+				className="name"
+				href="https://github.com/GabriWar"
+				onMouseOver={() => {
+					animateonmouseover("animate", "Gabriel", "Guerra");
+				}}
+			>
+				GABRIWAR
+			</div>
+			{bg}
+		</div>
 	);
 }
 
